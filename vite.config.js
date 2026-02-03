e57b2046ee66d7bafe90249dd2bd0ad68e55337d
Sikhosana24 <ambrosiasikhosana@gmail.com>
2026-02-03 07:12:55 +0200

    fix: alias react scheduler for netlify build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { createRequire } from "node:module"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      react: createRequire(import.meta.url).resolve("react"),
+      "react-dom": createRequire(import.meta.url).resolve("react-dom"),
+      scheduler: createRequire(import.meta.url).resolve("scheduler"),
+    },
+  },
 })
